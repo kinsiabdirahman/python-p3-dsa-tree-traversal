fcf7e3d7133d5edfcb3647027a3766b7f22d96d5
kinsiabdirahman <kinsiabdirahman@outlook.com>
2024-03-23 02:32:04 +0300

    Assignment Completed

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,17 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        def depth_first_traversal(node):
+            if node is None:
+                return None
+            if node['id'] == id:
+                return node
+            for child in node['children']:
+                result = depth_first_traversal(child)
+                if result:
+                    return result
+            return None
+
+        return depth_first_traversal(self.root)
